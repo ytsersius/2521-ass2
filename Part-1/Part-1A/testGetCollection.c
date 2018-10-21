55a67903e1446bedcf80b9e3ad46d2e538eae212
ying <ying@icumulus.com.au>
2018-10-21 14:04:19 +1100

    Get Collection test 0.2

--- Part-1/Part-1A/testGetCollection.c
+++ Part-1/Part-1A/testGetCollection.c
@@ -5,8 +5,26 @@
 #include <string.h>
 #include <ctype.h>
 
-#include "readData.h"
 #include "setList.h"
+
+Set GetCollection(void) {
+    FILE *collection = fopen("collection.txt", "r");
+    Set url_list = newSet();
+
+    char temp[100];
+    // I'm not sure how to dynamically allocate memory here
+
+    // Read the url IDs into a variable
+    while (fscanf(collection,"%s", temp) != EOF) {
+        char *url_id = strdup(temp);
+        SetInsert(url_list, url_id);
+    }
+
+    //free (url_id); // Do we need this?
+    fclose(collection);
+
+    return url_list;
+}
 
 int main()  {
     Set s = GetCollection();
