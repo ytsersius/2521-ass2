653472a91b2530b0d5ef237517757541fbc16981
ytsersius <sdeng24@yahoo.com.au>
2018-10-25 17:03:29 +1100

    completed calculate scalefootrule functions (haven't tested)

--- Part-3/scaledFootrule.c
+++ Part-3/scaledFootrule.c
@@ -26,6 +26,8 @@ int main(int argc, char* argv[]) {
 
     int *T_s = TSize(argc, argv);
 
+    
+
     return 0;
 }
 
@@ -85,13 +87,16 @@ int *TSize(int argc, char *argv[]) {
 }
 
 int GetTRank(char *T, char *c) {
+
     int rank = 1;
+    int found = 0;
 
     FILE *f = fopen(T, "r");
 
     char line[1000];
     while (fscanf(f, "%s", line) != EOF) {
         if (strcmp(line, c) == 0) {
+            found = 1;
             break;
         }
         rank ++;
@@ -99,17 +104,67 @@ int GetTRank(char *T, char *c) {
 
     fclose(f);
 
+    // if the url c is not in file T[i], return 0
+    if (found == 0) {
+        return 0;
+    }
+
     return rank;
 }
 
+// haven't tested
 double calculateTotalSFR(char *argv[], Set Collection, int *T_size,
     int *p) {
 
-        
+    double totalSFR = 0;
+    int n = Collection->nelems;
 
+    int c = 0;
+    // Go through all the nodes 1..n
+    while (c < n) {
+        char *url = GetURL(Collection, c);
+        int i;
+        // for every given rank file
+        for (i = 1; i < argc; i ++) {
+            int T_c = GetTRank(argv[i], url);
+            double distance = calculateScaledFootrule(T_c, T_size[i], p[c], n);
+            totalSFR = totalSFR + distance;
+        }
+
+        c ++;
     }
 
+    return totalSFR;
+}
+
+// haven't tested
 double calculateScaledFootrule(int T_c, int T_size, int p, int n) {
+    // if c is not in the current T, don't sum
+    if (T_c == 0) {
+        return 0;
+    }
 
+    if (p == 0 || n == 0 || T_size == 0) {
+        printf("error, invalid parameters in calculate SFR \n");
+        return;
+    }
 
+    double SFRDistance = 0;
+
+    SFRDistance = abs((T_c/T_size) - (p/n));
+
+    return SFRDistance;
+}
+
+// Given an index c get the url at that index of the set
+char *GetURL(Set Collection, int c) {
+    Node *curr = Collection->elems;
+    while (curr != NULL) {
+        if (curr->id == c) {
+            return curr->url;
+        }
+        curr = curr->next;
+    }
+
+    return NULL;
 }
